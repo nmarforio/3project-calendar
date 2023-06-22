c6721bfa87a51f0498e68d2bedbf368baf4a0278
python-game <n.marforio@gmail.com>
2023-06-22 13:08:27 +0200

    trying to fix the onClick event

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Calendar from 'react-calendar';
 
-function App() {
+
+
+const App : React.FC <{}> =() => {
+  const [day, setDay] = useState <Date>(new Date())
+  const correntYear: number = new Date().getFullYear()
+
+  
+
+  console.log(day)
   return (
     <div className="App">
+      <title>Calendar</title>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Calendar {correntYear}</h1>
       </header>
+      <div className='calendar-container'>
+        <Calendar onClickDay={()=>setDay} value={day}/>
+      </div>
+      <p className='Calendar-Day'> 
+      <span className='bold'>Selected Date:</span> {day.toDateString()}</p>
     </div>
   );
 }
