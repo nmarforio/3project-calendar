4700c7fbbf2dc3dd7e7651f5b279f50eda2ea1c0
python-game <n.marforio@gmail.com>
2023-06-27 12:34:55 +0200

    adding text area to the day and the mood selection

--- src/Component/Daytime.tsx
+++ src/Component/Daytime.tsx
@@ -1,11 +1,40 @@
+import { useState } from "react"
+import { Value } from "react-calendar/dist/cjs/shared/types"
 
+ export interface Props{
+    day: Value
+}
 
-const Daytime : React.FC <{}> = () : any =>{
+const Daytime : React.FC <{day: Value}>= ({day}: Props)  =>{
+    const [mood, setMood] = useState <boolean>(false)
+
+    const handleMood = (e : React.MouseEvent)=>{
+        e.preventDefault()
+        setMood(!mood)
+    }
+    console.log(mood)
+    const handleGreenClick =(e: React.MouseEvent)=>{
+        e.preventDefault()
+        setMood(false)
+
+    }
+    
+
     return(
         <>
-        <div>
-            
+        <form className="DayForm">
+        <div className="textarea">
+            <h2>Write down your toughts</h2>
+            <h4>{day?.toLocaleString()}</h4>
+            <textarea placeholder="how is your day?"></textarea>
         </div>
+        <button onClick={(e)=>handleMood(e)}>Choose your mood</button>
+        {mood ? <ul>
+            <button onClick={(e)=>handleGreenClick(e)} className="greenButton">Happy</button>
+            <button className="yellowButton">Ok</button>
+            <button className="redButton">Low</button>
+        </ul> : <></>}
+        </form>
         </>
     )
 }
